6fee2f5d580200e528d71ab8ce027a7047c24269
sudha0351 <sudhaists2006@gmail.com>
2025-06-30 10:26:33 +0530

    Create if.py

---
+++ if.py
@@ -0,0 +1,12 @@
+
+a = 10
+b = 20
+c = 30
+if a==10 and c==30:
+    print("the outer if executed")
+    if a==10:
+        print("the inner if executed")
+    else :
+        print("the inner else executed") 
+else:
+    print("the outer else executed")
