1735472e156723dc40e674b02cb2746686866975
sudha0351 <sudhaists2006@gmail.com>
2025-07-03 12:43:48 +0530

    Add files via upload

---
+++ listmethod.py
@@ -0,0 +1,21 @@
+""""
+list method:append/extend/count/stablity/index
+syntax/;listname.methodname()
+"""
+#append:this method is used to add the elements at the end 
+list=["editing","scaland","lodan"]
+print(list)
+list.append("brinigan")
+print(list)
+
+
+#extend:this method is used to add the elements at the end but
+#as a sudlist 
+list.extend(("paris","mata","as"))
+print(list)
+
+
+#count:this extend count the numder of repeated elements in a list
+flowers=["rose","mary","rose","jasime"]
+flowers.count(("rose"))
+print(flowers)
