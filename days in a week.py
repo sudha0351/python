f19e9416f944b49bfe25cfcc71a36f9d3d2fc740
sudha0351 <sudhaists2006@gmail.com>
2025-07-08 10:37:13 +0530

    Add files via upload

---
+++ days in a week.py
@@ -0,0 +1,19 @@
+# write a program to display the week names by taking input from thr user?
+
+day=int(input("Enter input.."))
+if day==0:
+    print("The day is sunday")
+elif day==1:
+    print("The day is monday")
+elif day==2:
+    print("The day is tuesday")
+elif day==3:
+    print("The day is wednesday")
+elif day==4:
+    print("The day is thursday")
+elif day==5:
+    print("The day is friday")
+elif day==6:
+    print("The day is saturday")
+else:
+    print("invalid input")
