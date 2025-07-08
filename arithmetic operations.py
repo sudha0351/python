0868d7d4809e3afef7a6dd3e697917e8db950a00
sudha0351 <sudhaists2006@gmail.com>
2025-07-08 10:34:57 +0530

    Add files via upload

---
+++ arithmetic operations.py
@@ -0,0 +1,30 @@
+""" 
+An operator is a special symbol which is used to perform an operation 
+eg: c=a+b 
+here c, a ,b are operands 
++ is a special symbol 
+The python operators are 
+1. Arithmetic Operator 
+2. Relational Operator 
+3. Logical Operator 
+4. Bitwise Operator 
+5. Identity Operator 
+6. Membership Operator 
+7. Assignment Operator
+An Arithmetic operator is used to perform some mathematical/arithmetic Operations 
+the operators are +,-,*,/,//,%,** 
+Note: input()is a function which takes input from user 
+""" 
+a = int(input("enter a number: ")) 
+b = int(input("enter a number: ")) 
+print("The addition ", a+b) 
+print("The subtraction ", a-b) 
+print("The mutliplication ", a*b) 
+print("The division ", a/b)#which returns the quotient 
+print("The floordivision ", a//b)#which returns the integral quotient part in a division 
+print("The modulardivision ", a%b)#which returns the remainder part before the decimal 
+print("The power ",a**b)#which returns the power 
+""" 
+//#which returns the integral quotient part in a division 
+%#which returns the remainder part before the decimal 
+"""
